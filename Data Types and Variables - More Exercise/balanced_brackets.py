3655ece1642b6b60b074eb0002c7486fd60179d4
Easterica <penev33@gmail.com>
2022-06-01 12:00:13 +0300

    rock paper scissors update

--- Data Types and Variables - More Exercise/balanced_brackets.py
+++ Data Types and Variables - More Exercise/balanced_brackets.py
@@ -1,15 +1,30 @@
 lines = int(input())
 parentheses_counter = 0
+is_balanced = False
 
 for i in range(lines):
     str_input = input()
 
-    if str_input == "(" or str_input == ")":
+    if str_input == "(":
         parentheses_counter += 1
-    else:
         continue
+    if str_input == ")" and parentheses_counter == 1:
+        is_balanced = True
+        parentheses_counter = 0
 
-if parentheses_counter % 2 == 0:
+if is_balanced:
     print("BALANCED")
 else:
     print("UNBALANCED")
+
+#
+#     if str_input == "(" or str_input == ")":
+#         parentheses_counter += 1
+#     else:
+#         continue
+#
+# if parentheses_counter % 2 == 0:
+#     print("BALANCED")
+# else:
+#     print("UNBALANCED")
+
--- Mini Projects/rock_paper_scissors.py
+++ Mini Projects/rock_paper_scissors.py
@@ -1,7 +1,5 @@
 import random
 
-computer_options = ["rock", "paper", "scissors"]
-computer_choice = computer_options[random.randint(0,2)]
 
 print("Welcome to the game \"Rock, Paper, Scissors\"")
 print("Please select one of the three options and write it down.")
@@ -9,31 +7,54 @@ print("Rock, Paper, Scissors?")
 
 my_choice = input().lower()
 
+computer_options = ["rock", "paper", "scissors"]
+computer_choice = computer_options[random.randint(0,2)]
+
 while my_choice not in computer_options:
     print("Please select one of the following options: Rock, Paper, Scissors")
     my_choice = input().lower()
+if my_choice in computer_options:
 
     if my_choice == "rock" and computer_choice == "scissors":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("You won!")
+
     elif my_choice == "rock" and computer_choice == "paper":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("You lost!")
+
     elif my_choice == "rock" and computer_choice == "rock":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("Draw!")
+
     elif my_choice == "paper" and computer_choice == "paper":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("Draw!")
+
     elif my_choice == "paper" and computer_choice == "rock":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("You won!")
+
     elif my_choice == "paper" and computer_choice == "scissors":
         print(f"Your choice is: {my_choice}")
         print(f"Computer's choice is: {computer_choice}")
         print("You lost!")
+    elif my_choice == "scissors" and computer_choice == "scissors":
+        print(f"Your choice is: {my_choice}")
+        print(f"Computer's choice is: {computer_choice}")
+        print("Draw!")
+    elif my_choice == "scissors" and computer_choice == "paper":
+        print(f"Your choice is: {my_choice}")
+        print(f"Computer's choice is: {computer_choice}")
+        print("You won!")
+    elif my_choice == "scissors" and computer_choice == "rock":
+        print(f"Your choice is: {my_choice}")
+        print(f"Computer's choice is: {computer_choice}")
+        print("You lost!")
+
+        
